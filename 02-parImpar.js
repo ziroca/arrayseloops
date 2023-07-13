52e77e3ebf8f113888271cdbc6257e176690c606
ziroca <eurilanne@gmail.com>
2023-07-13 16:57:34 -0300

    Resolução do exercício 02

---
+++ 02-parImpar.js
@@ -0,0 +1,9 @@
+const jogada1 = 3;
+const jogada2 = 4;
+
+const partida = jogada1 + jogada2;
+const jogada = 2;
+if (partida % jogada == 0) {
+    console.log("Par");
+} else
+    console.log("Ímpar");
